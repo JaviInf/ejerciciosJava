f90afe5890d1c648374410eb91519ee8b13f679a
JaviInf <jinfanteizk@gmail.com>
2014-04-22 12:46:13 +0200

    commit ejercicio 16

--- EjerciciosJava/src/com/javi/arrays/Ej16_01.java
+++ EjerciciosJava/src/com/javi/arrays/Ej16_01.java
@@ -1,5 +1,7 @@
 package com.javi.arrays;
 
+import java.util.Scanner;
+
 public class Ej16_01 {
 
 	/**
@@ -7,7 +9,24 @@ public class Ej16_01 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Introduce 10 numeros");
+		Scanner entrada=new Scanner(System.in);
+		int vector[]=new int[10];
+		for (int i=0; i<10; i++){
+			int numero=entrada.nextInt();
+			vector[i]=numero;
+		}
+		
+		int max=0;
+		int min=9999999;
+		for(int i=0; i<vector.length; i++) {
+			System.out.println( "elemento "+i+ " "+vector[i]);
+			if(vector[i]>=max) max=vector[i];
+		    if (min>vector[i])  min=vector[i];
+		}
+		
+		System.out.println("Numero maximo "+ max);
+		System.out.println("Numero minimo "+ min);
 	}
-
+	
 }
