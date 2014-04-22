d49900d5715435b5f87189aaba2e1be45ff2f9a9
JaviInf <jinfanteizk@gmail.com>
2014-04-22 12:27:04 +0200

    commit ejercicio 15 acierta el numero

---
+++ EjerciciosJava/src/com/javi/bucles/Ej15_01.java
@@ -0,0 +1,38 @@
+package com.javi.bucles;
+
+import java.util.Scanner;
+
+public class Ej15_01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aleatorio  = (int) (100*Math.random()+1);
+		System.out.println("Numero secreto"+aleatorio);
+		
+		int cont=0;
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduce el numero: ");
+		
+		int numero = entrada.nextInt();
+		Boolean acertado=false;
+		
+		while (cont<4 && !acertado) {
+			if(numero==aleatorio) {
+				System.out.println("Premio!!! has acertado el numero secreto");
+				acertado=true;
+			}
+			else
+			{
+				cont++;
+				System.out.println("Error: introduce otro, te quedan " + (5-cont) +"intentos");
+				numero = entrada.nextInt();
+			}
+			
+		}
+	}
+
+}
