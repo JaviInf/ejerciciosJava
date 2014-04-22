019390876510e0f5387411e4901985e97bf02d7a
JaviInf <jinfanteizk@gmail.com>
2014-04-22 12:03:46 +0200

    commit ejercicio 9 factorial

---
+++ EjerciciosJava/src/bucles/Ej09_01.java
@@ -0,0 +1,23 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ej09_01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduce el numero: ");
+		int numero= entrada.nextInt();
+		int factorial = 1;
+	
+		while ( numero!=0) {
+			  factorial=factorial*numero;
+			  numero--;
+			}
+		System.out.println("El factorial del numero es: "+factorial);
+	}
+}
