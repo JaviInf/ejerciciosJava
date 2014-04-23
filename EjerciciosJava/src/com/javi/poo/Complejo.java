c2b5d9e4d1c1bf28cacdc4ac679f7db4a68d5ce6
JaviInf <jinfanteizk@gmail.com>
2014-04-23 11:41:09 +0200

    commit ejercicio 25 Clases complejo

---
+++ EjerciciosJava/src/com/javi/poo/Complejo.java
@@ -0,0 +1,35 @@
+package com.javi.poo;
+
+public class Complejo {
+
+	private double pReal;
+	private double pImag;
+	
+	public Complejo() {
+	}
+
+	public Complejo( double x, double y) {
+		this.pReal=x;
+		this.pImag=y;
+	}
+
+	public void asignar(double x, double y){
+		this.pReal=x;
+		this.pImag=y;
+	}
+	public void sumar(Complejo b){
+		this.pReal=this.pReal+b.pReal;
+		this.pImag=this.pImag+b.pImag;
+	}
+	
+	public String toString(){
+		return this.pReal+", "+this.pImag;
+		
+	}
+	
+	public static  Complejo sumar(Complejo a, Complejo b){
+		return a;
+		
+	}
+	
+}
