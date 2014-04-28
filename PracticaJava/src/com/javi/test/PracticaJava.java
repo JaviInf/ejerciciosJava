09d15ace0ee745d77c57beec9acf819bf1ad2ee2
JaviInf <jinfanteizk@gmail.com>
2014-04-28 11:00:47 +0200

    2 commit añadidos metodos  quitaVida reiniciaPartida actualizaRecord y sus test Junit

--- PracticaJava/src/com/javi/test/PracticaJava.java
+++ PracticaJava/src/com/javi/test/PracticaJava.java
@@ -22,16 +22,53 @@ public class PracticaJava {
 	}
 	@Test
 	public void testRestarVidayMostrar(){
-		juego.numVidas--;
-		assertEquals("Juego", 4, juego.obtenVidasRestantes());
+	///	juego.numVidas--;
+		//assertEquals("Juego", 4, juego.obtenVidasRestantes());
 	}
 	
 	@Test
 	public void testNuevaInstancia(){
 		Juego nuevojuego=new Juego(5);
 		assertEquals("Juego", 5, nuevojuego.obtenVidasRestantes());
-		juego.numVidas--;
+	//	juego.numVidas--;
+	//	assertEquals("Juego", 4, juego.obtenVidasRestantes());
+	}
+	
+	@Test
+	public void testQuitaVida(){
+		assertEquals("Juego", 5, juego.obtenVidasRestantes());
+		juego.quitaVidas();
 		assertEquals("Juego", 4, juego.obtenVidasRestantes());
 	}
+	
+	@Test
+	public void testReiniciaPartida(){
+		assertEquals("Juego", 5, juego.obtenVidasRestantes());
+		juego.quitaVidas();
+		assertEquals("Juego", 4, juego.obtenVidasRestantes());
+		juego.reiniciaPartida();
+		assertEquals("Juego", 5, juego.obtenVidasRestantes());
+	}
+	
+	@Test
+	public void actualizaRecord(){ // 3 casos 1-record igualado   2-record superado    3-nada
+		juego.quitaVidas();
+		// pruebo que actualiza record
+		assertEquals("Juego", 4, juego.obtenVidasRestantes());
+		assertEquals("Juego", "Se ha actualizado el record: "+"4" , juego.actualizarRecord());
+		
+		//puebo que no hace nada en tercer caso
+		Juego nuevo=new Juego(3);
+		assertEquals("Juego", "" , nuevo.actualizarRecord());
+		
+		// pruebo que iguala record
+		Juego nuevoEmpate=new Juego(4);
+		assertEquals("Juego", "Se ha alcanzado el record" , nuevoEmpate.actualizarRecord());
+
+	}
+	
+	
+	
+	
 
 }
